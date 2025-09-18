1acd675538656b7a5ea555101c1f3c6ad400f18e
rajmakvana143 <rajmakvana143e@gmail.com>
2025-09-18 14:54:44 +0530

    ohk

--- server/utils/getAvatar.js
+++ server/utils/getAvatar.js
@@ -1,5 +1,6 @@
+const randomName =  `${Math.floor(Math.random() * 1000)}`;
 
 export async function getAvatar() {
-    const avatarUrl = await fetch('https://avatar.iran.liara.run/public/boy');
+    const avatarUrl = await fetch(`https://avatar-placeholder.iran.liara.run/?username=${randomName}`);
     return avatarUrl;
 }

7371ca6a306a1252453b2cbf0183c40c0e861d79
rajmakvana143 <rajmakvana143e@gmail.com>
2025-09-18 22:37:02 +0530

    done

--- server/index.js
+++ server/index.js
@@ -11,18 +11,34 @@ import ReportRouter from "./routes/report.route.js";
 import getAllDataRouter from "./routes/dashboard.route.js";
 import PostRouter from "./routes/post.route.js"
 import UpdateRouter from "./routes/adminDashboard.route.js"
+import { authenticate } from "./middleware/authMiddleware.js";
+import User from "./models/user.model.js";
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 //router 
+
+app.get('/user' , authenticate , async (req , res) => {
+    const userId = req.user.id;
+
+    try {
+        const user = await User.findById(userId).select(" -password");
+        res.send({data : user});
+    } catch (error) {
+        res
+    .status(500)
+    .json({ success: false, message: "Server error", error: error.message });
+  
+    }
+});
+
 app.use('/api/auth' , UserRouter); // user login-Signup
 app.use('/api/report' , ReportRouter ); // user report create / show 
 app.use('/api/get' , getAllDataRouter ); // show all Users and Reports
 app.use('/api/posts' , PostRouter ); // CRUD in Post
-app.use('/api/report/update' , UpdateRouter)
-
+app.use('/api/report/update' , UpdateRouter);
 
 // connect to Database 
 connectDB();
